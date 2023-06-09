561531a728236fe1d443b182bce498f00b2e0bd8
Ali Eyvazli <ali.eyvazli@kellogg.northwestern.edu>
2023-06-09 00:24:23 +0000

    logic and count done / style and alerts missing

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,16 @@
 class LikesController < ApplicationController
-  def create
+  def create  
     @post = Post.find(params[:post_id])
     @like = @post.likes.build(user: current_user)
-    if @like.save
+
+    if @like.save  
       redirect_to root_path, notice: 'You liked this post.'
-    else
-      redirect_to root_path, alert: 'Unable to like this post.'
-    end
-  end
-end
+    else  
+      if @like.errors.include?(:post_id)  
+        redirect_to root_path, alert: 'You have already liked this post.'
+      else  
+        redirect_to root_path, alert: 'Unable to like this post.'
+      end  
+    end  
+  end  
+end  
